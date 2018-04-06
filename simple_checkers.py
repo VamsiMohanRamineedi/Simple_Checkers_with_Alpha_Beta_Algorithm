1407271ce7b29c2626c459ecc269c15904b0491b
VamsiMohanRamineedi <vmr286@nyu.edu>
2018-04-05 21:53:11 -0400

    Previous changes

--- simple_checkers.py
+++ simple_checkers.py
@@ -1,16 +1,19 @@
 class Board:
 
 	def __init__(self):
-		pass
-
-	def default_board(self):
-		# resets the boardstate to initial position
+		# sets the boardstate to starting condition
         # -1 = empty, 0=black, 1=white
-        self.boardState = [
-            [-1,1,-1,1,-1,1],
+		self.boardState = [[-1,1,-1,1,-1,1],
             [1,-1,1,-1,1,-1],
             [-1,-1,-1,-1,-1,-1],
             [-1,-1,-1,-1,-1,-1],
             [-1,0,-1,0,-1,0],
             [0,-1,0,-1,0,-1]]
+
+b = Board()
+print(b.boardState[5][1])
+
+        
+
+
        
